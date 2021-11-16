3d716d95098d9037de138eab94aa5b6e72948548
heladiofelix18@gmail.com <heladiofelix18@gmail.com>
2021-11-16 11:56:48 -0600

    TallerHeladio

---
+++ Taller_Heladio.api/Modelos/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_Heladio.api.Modelos
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> Options) :base(Options)
+        {
+
+        }
+    }
+}
---
+++ Taller_Heladio.api/Modelos/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller_Heladio.api.Modelos
+{
+    
+    public class Marca
+    {
+        [Key]
+        public int IdMarca { get; set; }
+        public int Nombre { get; set; }
+        [Required]
+        [MaxLength]
+        public bool Activo { get; set; }
+
+    }
+}
